0236ff919c7a16182f5bc5c911f28851695ab136
Thalisson-Alves <thalisson2030@gmail.com>
2023-12-28 22:19:42 -0300

    add cmp-luasnip

---
+++ lua/plugins/cmp-luasnip.lua
@@ -0,0 +1,5 @@
+return {
+  "saadparwaiz1/cmp_luasnip",
+  event = "BufReadPre",
+  dependencies = { "L3MON4D3/LuaSnip" },
+}
--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -3,9 +3,13 @@ local config = function()
   local luasnip = require("luasnip")
   local lspkind = require("lspkind")
 
-  require("luasnip/loaders/from_vscode").lazy_load({ include = {"c", "cpp", "python" } })
+  require("luasnip/loaders/from_vscode").lazy_load({ include = { "c", "cpp", "python" } })
 
   vim.opt.completeopt = "menu,menuone,noselect"
+
+  -- configure custom snippets
+  local custom_snips_opt = { base_dir = "~/prog/comp/Competitive-Programming/code", include_filetypes = { "cpp", "python" } }
+  require("util.custom-snips").get_and_add_snips(custom_snips_opt)
 
   cmp.setup({
     snippet = {
@@ -19,15 +23,15 @@ local config = function()
       ["<C-b>"] = cmp.mapping.scroll_docs(-4),
       ["<C-f>"] = cmp.mapping.scroll_docs(4),
       ["<C-Space>"] = cmp.mapping.complete(), -- show completion suggestions
-      ["<C-e>"] = cmp.mapping.abort(), -- close completion window
+      ["<C-e>"] = cmp.mapping.abort(),        -- close completion window
       ["<CR>"] = cmp.mapping.confirm({ select = false }),
     }),
     -- sources for autocompletion
     sources = cmp.config.sources({
       { name = "nvim_lsp" }, -- lsp
-      { name = "luasnip" }, -- snippets
-      { name = "buffer" }, -- text within current buffer
-      { name = "path" }, -- file system paths
+      { name = "luasnip" },  -- snippets
+      { name = "buffer" },   -- text within current buffer
+      { name = "path" },     -- file system paths
       -- { name = "crates" }, -- crates plugin
     }),
     -- configure lspkind for vs-code like icons
@@ -40,16 +44,16 @@ local config = function()
   })
 end
 return {
-	"hrsh7th/nvim-cmp",
-	config = config,
-	dependencies = {
-		"onsails/lspkind.nvim",
-		{
-			"L3MON4D3/LuaSnip",
-			-- follow latest release.
-			version = "2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
-			-- install jsregexp (optional!).
-			build = "make install_jsregexp",
-		},
-	},
+  "hrsh7th/nvim-cmp",
+  config = config,
+  dependencies = {
+    "onsails/lspkind.nvim",
+    {
+      "L3MON4D3/LuaSnip",
+      -- follow latest release.
+      version = "2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+      -- install jsregexp (optional!).
+      build = "make install_jsregexp",
+    },
+  },
 }
