27e6ac63dd9c1b185dad9ea5eb4dc0080b99fc2f
Thalisson-Alves <thalisson2030@gmail.com>
2023-12-30 20:24:54 -0300

    bind cwd to neo-tree

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -12,7 +12,7 @@ return {
     sources = { "filesystem", "buffers", "git_status", "document_symbols" },
     open_files_do_not_replace_types = { "terminal", "Trouble", "trouble", "qf", "Outline" },
     filesystem = {
-      bind_to_cwd = false,
+      bind_to_cwd = true,
       follow_current_file = { enabled = true },
       use_libuv_file_watcher = true,
     },
