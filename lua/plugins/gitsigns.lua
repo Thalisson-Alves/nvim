6aa82e98ef19b942e6d0f058175ae8701ad96c4c
Thalisson-Alves <thalisson2030@gmail.com>
2024-01-17 15:57:04 -0300

    make gitsigns ready on startup

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -43,9 +43,8 @@ return {
       enable = false,
     },
   },
+  lazy = false,
   keys = {
-    "<leader>g",
-    { "<leader>gg", "<cmd>Neogit<CR>", desc = "GitUI" },
     { "<leader>gj", "<cmd>lua require 'gitsigns'.next_hunk()<cr>", desc = "Next Hunk" },
     { "<leader>gk", "<cmd>lua require 'gitsigns'.prev_hunk()<cr>", desc = "Prev Hunk" },
     { "<leader>gl", "<cmd>lua require 'gitsigns'.blame_line()<cr>", desc = "Blame" },
