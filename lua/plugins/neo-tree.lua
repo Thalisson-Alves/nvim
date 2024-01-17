d74487fd42b9de90af0d021038dffe58d859b51d
Thalisson-Alves <thalisson2030@gmail.com>
2024-01-17 20:16:14 -0300

    fix neo-tree dependencies

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,7 +1,7 @@
 return {
   "nvim-neo-tree/neo-tree.nvim",
   branch = "v3.x",
-  requires = {
+  dependencies  = {
     "nvim-lua/plenary.nvim",
     "nvim-tree/nvim-web-devicons",
     "MunifTanjim/nui.nvim",
