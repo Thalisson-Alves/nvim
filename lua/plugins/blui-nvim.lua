5e43a9b802513581516f18a541f8cb492735c1dd
Thalisson-Alves <thalisson2030@gmail.com>
2024-02-18 11:41:33 -0300

    using blui from path

--- lua/plugins/blui-nvim.lua
+++ lua/plugins/blui-nvim.lua
@@ -1,5 +1,6 @@
 return {
-  "Thalisson-Alves/blui-nvim",
+  -- "Thalisson-Alves/blui-nvim",
+  dir = "~/prog/nvim-plugins/blui-nvim",
   dependencies = {
     "akinsho/bufferline.nvim",
     "nvim-lua/plenary.nvim",
